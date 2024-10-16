3d0100490560177666336e418783a20e32a965a9
Derek Johnson <hello@imderek.com>
2024-10-15 22:29:04 -0400

    tailwind config: dynamic primary color var

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
@@ -11,6 +12,21 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: {
+          50: colors.emerald[50],
+          100: colors.emerald[100],
+          200: colors.emerald[200],
+          300: colors.emerald[300],
+          400: colors.emerald[400],
+          500: colors.emerald[500],
+          600: colors.emerald[600],
+          700: colors.emerald[700],
+          800: colors.emerald[800],
+          900: colors.emerald[900],
+          950: colors.emerald[950],
+        },
       },
     },
   },
