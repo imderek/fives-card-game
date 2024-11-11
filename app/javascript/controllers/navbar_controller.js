8e959cc8eba0b9bc59b1d8924de3f14b8df0946a
Derek Johnson <hello@imderek.com>
2024-11-11 11:58:20 -0500

    updates

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -3,26 +3,26 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
-  static targets = ["menu", "navItem"]
+  // static targets = ["menu", "navItem"]
 
-  connect() {
-    console.log('Menu controller connected')
-    // console.log(this.navItemTarget.offsetLeft)
-    // console.log(this.menuTarget.offsetLeft)
-    // this.menuTarget.style.left = this.navItemTarget.offsetLeft + 'px';
-    this.menuTarget.style.marginLeft = '-15.5px'
-    // this.closeMenu()
-  }
+  // connect() {
+  //   console.log('Menu controller connected')
+  //   // console.log(this.navItemTarget.offsetLeft)
+  //   // console.log(this.menuTarget.offsetLeft)
+  //   // this.menuTarget.style.left = this.navItemTarget.offsetLeft + 'px';
+  //   this.menuTarget.style.marginLeft = '-15.5px'
+  //   // this.closeMenu()
+  // }
 
-  openMenu() {
-    // Remove classes that hide the menu
-    this.menuTarget.classList.remove('opacity-0', 'pointer-events-none');
-    this.menuTarget.classList.add('opacity-100');
-  }
+  // openMenu() {
+  //   // Remove classes that hide the menu
+  //   this.menuTarget.classList.remove('opacity-0', 'pointer-events-none');
+  //   this.menuTarget.classList.add('opacity-100');
+  // }
 
-  closeMenu() {
-    // Add classes to hide the menu with a transition
-    this.menuTarget.classList.remove('opacity-100');
-    this.menuTarget.classList.add('opacity-0', 'pointer-events-none');
-  }
+  // closeMenu() {
+  //   // Add classes to hide the menu with a transition
+  //   this.menuTarget.classList.remove('opacity-100');
+  //   this.menuTarget.classList.add('opacity-0', 'pointer-events-none');
+  // }
 }
