d9da32f5d85e8229ed0ded8299a956c4b80068f6
Derek Johnson <hello@imderek.com>
2024-11-14 12:02:33 -0500

    add and install devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_14_165409) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_14_170145) do
   create_table "organization_memberships", force: :cascade do |t|
     t.integer "organization_id", null: false
     t.integer "user_id", null: false
@@ -52,7 +52,12 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_14_165409) do
     t.boolean "is_active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "organization_memberships", "organizations"
