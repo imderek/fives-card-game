b0ffac5b562367288539720d9cbbc50433f54b38
Derek Johnson <hello@imderek.com>
2024-12-11 10:30:34 -0500

    refactor gamescontroller#play_card

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -37,71 +37,28 @@ class GamesController < ApplicationController
   def play_card
     respond_to do |format|
       format.turbo_stream do
-        if current_user.id == @game.current_turn && @game.turn_phase == "play_card"
-          card = params[:card].to_unsafe_h.symbolize_keys
-          card[:player_id] = current_user.id
-          
-          if @game.valid_move?(card)
-            # Remove card from player's hand
-            current_hand = current_user.id == @game.player1_id ? :player1_hand : :player2_hand
-            @game[current_hand] = @game[current_hand].reject { |c| c[:suit] == card[:suit] && c[:value] == card[:value] }
-            
-            # Get current board state
-            board_state = GameBoardSerializer.new(@game).as_json
-            
-            # Determine which player's columns to update
-            player_key = current_user.id == @game.player1_id ? :player_1 : :player_2
-            
-            # Add card to the appropriate column
-            board_state[player_key][:columns][card[:column].to_i][:cards] << "#{card[:value]}#{card[:suit].first.upcase}"
-            
-            # Update the game's board_cards with the new state
-            @game.board_cards = []
-            [:player_1, :player_2].each do |player_key|
-              player_id = player_key == :player_1 ? @game.player1_id : @game.player2_id
-              board_state[player_key][:columns].each_with_index do |column, column_index|
-                column[:cards].each do |card_code|
-                  value = card_code[0..-2]
-                  suit = card_code[-1].downcase
-                  @game.board_cards << {
-                    suit: suit,
-                    value: value,
-                    player_id: player_id,
-                    column: column_index
-                  }
-                end
-              end
-            end
+        unless valid_turn?
+          render_error("Not your turn or wrong phase") and return
+        end
 
-            # Auto-draw a card
-            drawn_card = @game.deck.pop
-            @game[current_hand] = @game[current_hand] + [drawn_card.symbolize_keys]
-            
-            # Switch turn to other player and set phase
-            @game.current_turn = @game.player2_id || @game.player1_id
-            @game.turn_phase = :play_card
-            
-            if @game.save
-              render turbo_stream: [
-                turbo_stream.replace("game-state", partial: "games/game_state", locals: { game: @game, current_user: current_user }),
-                turbo_stream.replace("game-status", partial: "games/game_status", locals: { game: @game, current_user: current_user }),
-                turbo_stream.replace("player-controls", partial: "games/player_controls", locals: { game: @game, current_user: current_user })
-              ]
-            else
-              render turbo_stream: turbo_stream.update("game_error", "Failed to update game")
-            end
-          else
-            render turbo_stream: turbo_stream.update("game_error", "Cannot add more than 5 cards to a column")
-          end
+        card = params[:card].to_unsafe_h.symbolize_keys.merge(player_id: current_user.id)
+        
+        unless @game.valid_move?(card)
+          render_error("Cannot add more than 5 cards to a column") and return
+        end
+
+        if play_card_and_update_game(card)
+          render_success_response
         else
-          render turbo_stream: turbo_stream.update("game_error", "Not your turn or wrong phase")
+          render_error("Failed to update game")
         end
       end
     end
   rescue => e
     Rails.logger.error "Error in play_card: #{e.message}\n#{e.backtrace.join("\n")}"
-    render turbo_stream: turbo_stream.update("game_error", e.message)
+    render_error(e.message)
   end
+
 
   def destroy
     @game = Game.find(params[:id])
@@ -134,5 +91,69 @@ class GamesController < ApplicationController
     current_hand = current_user.id == @game.player1_id ? :player1_hand : :player2_hand
     @game[current_hand] << drawn_card
     @game.save
+  end
+
+  private
+
+  def valid_turn?
+    current_user.id == @game.current_turn && @game.turn_phase == "play_card"
+  end
+
+  def play_card_and_update_game(card)
+    # Remove played card from hand
+    current_hand = current_user.id == @game.player1_id ? :player1_hand : :player2_hand
+    @game[current_hand] = @game[current_hand].reject do |c| 
+      c[:suit] == card[:suit] && c[:value] == card[:value]
+    end
+
+    # Update board state
+    update_board_state(card)
+    
+    # Draw new card
+    drawn_card = @game.deck.pop
+    @game[current_hand] = @game[current_hand] + [drawn_card.symbolize_keys]
+    
+    # Update turn
+    @game.current_turn = @game.player2_id || @game.player1_id
+    @game.turn_phase = :play_card
+    
+    @game.save
+  end
+
+  def update_board_state(played_card)
+    board_state = GameBoardSerializer.new(@game).as_json
+    player_key = current_user.id == @game.player1_id ? :player_1 : :player_2
+    
+    # Add card to column
+    board_state[player_key][:columns][played_card[:column].to_i][:cards] << 
+      "#{played_card[:value]}#{played_card[:suit].first.upcase}"
+    
+    # Convert board state back to board_cards array
+    @game.board_cards = []
+    [:player_1, :player_2].each do |player_key|
+      player_id = player_key == :player_1 ? @game.player1_id : @game.player2_id
+      board_state[player_key][:columns].each_with_index do |column, column_index|
+        column[:cards].each do |card_code|
+          @game.board_cards << {
+            suit: card_code[-1].downcase,
+            value: card_code[0..-2],
+            player_id: player_id,
+            column: column_index
+          }
+        end
+      end
+    end
+  end
+
+  def render_success_response
+    render turbo_stream: [
+      turbo_stream.replace("game-state", partial: "games/game_state", locals: { game: @game, current_user: current_user }),
+      turbo_stream.replace("game-status", partial: "games/game_status", locals: { game: @game, current_user: current_user }),
+      turbo_stream.replace("player-controls", partial: "games/player_controls", locals: { game: @game, current_user: current_user })
+    ]
+  end
+
+  def render_error(message)
+    render turbo_stream: turbo_stream.update("game_error", message)
   end
 end
