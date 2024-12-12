2a4193a816ebff5fbb333e0a4e285796b0b58923
Derek Johnson <hello@imderek.com>
2024-12-12 10:54:11 -0500

    override devise controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :owned_deals, class_name: 'Deal', foreign_key: 'owner_id'
+
+  # Skip password validation
+  def password_required?
+    false
+  end
+
+  def update_with_password(params, *options)
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+    update_without_password(params, *options)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
   resources :deals
-  # get "home/show"
-  devise_for :users
+
+  # Devise routes
+  # --------------
+  devise_for :users, skip: [:sessions, :registrations]
+  devise_scope :user do
+    get '/users/sign_in', to: 'users/sessions#new', as: :new_user_session
+    post '/users/sign_in', to: 'users/sessions#create', as: :user_session
+    get '/users/sign_up', to: 'users/registrations#new', as: :new_user_registration
+    post '/users', to: 'users/registrations#create', as: :user_registration
+    delete '/users/sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
+  end
+  # --------------
+
   get "dashboard", to: "dashboards#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
