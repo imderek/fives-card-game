371d895fac69479c590b162d86bf86c78457ef4c
Derek Johnson <hello@imderek.com>
2024-12-25 21:49:06 -0500

    disable column bg animations for now; add glow to highest hands

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -3,20 +3,20 @@ module GamesHelper
     base_classes = "min-w-[4.5rem] min-h-[14.25rem] p-2 relative column shadow-lg transition-all duration-150 flex flex-col gap-1 w-full"
     
     strength_classes = case score
-    when 1000, 800..999  # Royal Flush & Straight Flush
-      "bg-gradient-to-r from-purple-600/60 via-purple-500/30 to-purple-600/60 ring-2 ring-purple-500 animate-shimmer bg-[length:200%_100%]"
-    when 700..799, 600..699  # Quads & Full House
-      "bg-gradient-to-r from-red-600/60 via-red-500/30 to-red-600/60 ring-2 ring-red-500 animate-shimmer bg-[length:200%_100%]"
+    when 1000, 700..999  # Royal Flush & Straight Flush & Quads
+      "bg-gradient-to-br from-purple-600/80 via-purple-800/30 via-35% to-purple-600/60 ring-1 ring-purple-500 bg-[length:200%_100%] shadow-[0_0_25px_7px_rgba(168,85,247,0.5)] "
+    when 600..699  # Full House
+      "bg-gradient-to-br from-red-600/80 via-red-800/30 via-35% to-red-600/60 ring-1 ring-red-500 bg-[length:200%_100%] shadow-[0_0_18px_4px_rgba(220,38,38,0.5)]"
     when 500..599  # Flush
-      "bg-gradient-to-r from-amber-600/60 via-amber-500/30 to-amber-600/60 ring-2 ring-amber-500 animate-shimmer bg-[length:200%_100%]"
+      "bg-gradient-to-br from-amber-600/80 via-amber-800/30 via-35% to-amber-600/60 ring-1 ring-amber-500 bg-[length:200%_100%]"
     when 400..499  # Straight
-      "bg-amber-600/60 ring-2 ring-amber-500"
+      "bg-amber-600/60 ring-1 ring-amber-500"
     when 300..399  # Three of a Kind
-      "bg-green-600/60 ring-2 ring-green-500"
-    when 101..299  # Pairs
-      "bg-cyan-600/60 ring-2 ring-cyan-500"
-    when 50..100  # High Card
-      "bg-slate-600/60 ring-2 ring-slate-500"
+      "bg-green-600/60 ring-1 ring-green-500"
+    when 101..299  # Two Pair
+      "bg-cyan-700/50 ring-1 ring-cyan-600"
+    when 50..100  # Pair
+      "bg-slate-600/60 ring-1 ring-slate-500"
     else # High Card or no hand
       "bg-slate-600/60"
     end
@@ -26,9 +26,9 @@ module GamesHelper
 
   def score_color_class(score)
     case score
-    when 1000, 800..999  # Royal Flush & Straight Flush
+    when 1000, 700..999  # Royal Flush & Straight Flush & Quads
       "text-purple-400"
-    when 700..799, 600..699  # Quads & Full House
+    when 600..699  # Full House
       "text-red-400"
     when 500..599  # Flush
       "text-amber-400"
