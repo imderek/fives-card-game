2402de622fdce51599e29ef8eda0513954397491
Derek Johnson <hello@imderek.com>
2024-12-31 17:17:27 -0500

    slight anim adjustment

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
         },
         'scale-up': {
           '0%': { transform: 'scale(1.0)' },
-          '50%': { transform: 'scale(1.05)' },
+          '50%': { transform: 'scale(1.07)' },
           '100%': { transform: 'scale(1.0)' }
         }
       },
@@ -36,7 +36,7 @@ module.exports = {
         'card-enter': 'card-enter 500ms ease-out forwards',
         'enter-scale': 'enter-scale 200ms ease-out forwards',
         'shimmer': 'shimmer 4s linear infinite',
-        'scale-up': 'scale-up 300ms ease-out forwards'
+        'scale-up': 'scale-up 200ms ease-out forwards'
       },
       fontFamily: {
         sans: ['Poppins', ...defaultTheme.fontFamily.sans],
