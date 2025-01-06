3910576c982be3b2bbdb649939d8bb849c29567b
Derek Johnson <hello@imderek.com>
2025-01-06 11:55:00 -0500

    setup action cable to work with devise/current_user

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -1,10 +1,17 @@
 class GameChannel < ApplicationCable::Channel
   def subscribed
     game = Game.find(params[:game_id])
+    Rails.logger.info "GameChannel#subscribed: game_id=#{params[:game_id]}"
     stream_for game
+    Rails.logger.info "Successfully subscribed to game #{game.id}"
+  rescue => e
+    Rails.logger.error "Error in GameChannel#subscribed: #{e.message}"
+    Rails.logger.error e.backtrace.join("\n")
+    reject
   end
 
   def self.broadcast_update(game)
+    Rails.logger.info "Broadcasting update for game #{game.id}"
     broadcast_to(game, {
       game: game.as_json(
         methods: [
@@ -12,13 +19,16 @@ class GameChannel < ApplicationCable::Channel
           :player2_hand, 
           :board_cards,
           :player1_discard_pile,
-          :player2_discard_pile
+          :player2_discard_pile,
+          :deck,
+          :column_scores
         ]
       )
     })
+    Rails.logger.info "Finished broadcasting update for game #{game.id}"
   end
 
   def unsubscribed
-    # Cleanup when channel is unsubscribed
+    Rails.logger.info "GameChannel#unsubscribed"
   end
 end 
